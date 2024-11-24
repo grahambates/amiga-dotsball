2817315d48d36f201324dabf526c518942a0fa97
Graham Bates <info@grahambates.com>
2024-11-24 16:52:17 +0000

    Make js a bit more readable

--- gendata.js
+++ gendata.js
@@ -11,22 +11,23 @@ const TBL_SIZE = R * SIN_LEN * 2 * TBL_ITEM
 initCode()
 initTbl()
 
+// Generate draw code asm to stdout
 function initCode() {
   let count = 0
   const angles = []
 
+  // Generate random dot angles per line
   for (let y = 0; y < H; y++) {
-    const rowAngles = []
-    angles.push(rowAngles)
+    const lineAngles = []
+    angles.push(lineAngles)
     for (let i = 0; i < dotsPerLine; i++) {
-      rowAngles.push(
+      lineAngles.push(
         Math.floor(Math.random() * SIN_LEN)
       )
       count++
     }
   }
 
-  let i = 0
   let o = 0
 
   const doAngle = a => {
@@ -34,6 +35,7 @@ function initCode() {
     console.log(` bset.b d1,${o}(a1,d0)`)
   }
 
+  let i = 0
   for (let j = 0; j < R - 1; j++) {
     angles[i++].forEach(doAngle)
     o += 32
@@ -44,6 +46,7 @@ function initCode() {
     console.log(` adda.w d2,a0`)
   }
 
+  // Descending order for bottom half of screen
   for (let j = R - 2; j >= 0; j--) {
     console.log(` suba.w d2,a0`)
     angles[i++].forEach(doAngle)
@@ -56,25 +59,39 @@ function initCode() {
   console.log('; ' + count)
 }
 
+// Write table data to file
 function initTbl() {
   const tblBuf = Buffer.alloc(TBL_SIZE)
   let o = 0
 
+  // Generate 128 pre-scaled sin tables(!)
   for (let y = 0; y < R; y++) {
+    // Line width of circle at this y
     const _y = y - R
-    const w = Math.sqrt(R * R - _y * _y)
+    const lineWidth = Math.sqrt(R * R - _y * _y)
+
     for (let i = 0; i < SIN_LEN; i++) {
-      const a = i / SIN_LEN * Math.PI * 2
-      const v = Math.floor(Math.sin(a) * w + Math.random() / 2) + R
-      let shifted = v >> 3
-      if (a > Math.PI * 0.5 && a < Math.PI * 1.5) {
-        shifted += 256 * 256 / 8
+      const angle = i / SIN_LEN * Math.PI * 2
+      const v = Math.floor(
+        Math.sin(angle) * lineWidth +
+        Math.random() / 2 // Hide banding from low res
+      ) + R
+
+      // Write a pair of values to go into bset instruction
+      let byteOffset = v >> 3
+      // Additional offset for back facing dots
+      if (angle > Math.PI * 0.5 && angle < Math.PI * 1.5) {
+        // Bitplane size
+        byteOffset += 256 * 256 / 8
       }
-      const not = (~v) & 7
-      tblBuf.writeUInt16BE(shifted, o)
-      tblBuf.writeUInt16BE(not, o + 2)
-      tblBuf.writeUInt16BE(shifted, TBL_SIZE / 2 + o)
-      tblBuf.writeUInt16BE(not, TBL_SIZE / 2 + o + 2)
+      const bitToSet = (~v) & 7
+
+      tblBuf.writeUInt16BE(byteOffset, o)
+      tblBuf.writeUInt16BE(bitToSet, o + 2)
+      // Table is repeated to allow angle offset without modulo
+      tblBuf.writeUInt16BE(byteOffset, TBL_SIZE / 2 + o)
+      tblBuf.writeUInt16BE(bitToSet, TBL_SIZE / 2 + o + 2)
+
       o += TBL_ITEM
     }
   }
